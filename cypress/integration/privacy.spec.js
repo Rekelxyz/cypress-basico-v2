057d14edb0f6b1ece4956f801901aed99b984ca2
Paulo André <deders2002@gmail.com>
2024-09-16 09:28:32 -0300

    Create cypress project

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -3,3 +3,5 @@ it('testa a página da política de privacidade de forma independente', function
     cy.visit('./src/privacy.html')
     cy.contains('Talking About Testing').should('be.visible')
 })
+
+//Comentário
